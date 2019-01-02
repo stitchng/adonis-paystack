778ca277f449357ad3460d9c482cf94d6860aa1c
StitchNG <stitchnig@gmail.com>
2019-01-02 18:18:40 +0100

    Update PayStackProvider.js

--- providers/PayStackProvider.js
+++ providers/PayStackProvider.js
@@ -7,7 +7,7 @@ class PayStackProvider extends ServiceProvider {
     this.app.singleton('Adonis/Addons/PayStack', () => {
       const Config = this.app.use('Adonis/Src/Config')
       const PayStack = require('../src/Paystack')
-      return new PayStack(require('@stitchng/paystack'), Config)
+      return (new PayStack(require('@stitchng/paystack'), Config)).agent
     })
   }
 }
