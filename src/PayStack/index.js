bd5ee1db80820f62a3767bb5c65f7cca60a2a72f
StitchNG <stitchnig@gmail.com>
2019-01-02 18:16:08 +0100

    Update index.js

--- src/PayStack/index.js
+++ src/PayStack/index.js
@@ -1,1 +1,20 @@
-'use strict'
+'use strict';
+
+const Event = use('Event')
+
+class PayStackAPIClient {
+      constructor(Agent, Config) {
+          this.agent = new Agent(
+              Config.get('paystack.apiKey'),
+              Config.get('app.env')
+          );
+        
+          if(Config.get('paystack.installWebHook') === true){
+              this.agent.webhook = Agent.webhook.install(
+                  Event
+              )
+          }
+      }
+};
+
+module.exports = PayStackAPIClient
