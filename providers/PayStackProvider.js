08a7fae408d1c40f9e2930cc3ef29e11b16af203
StitchNG <stitchnig@gmail.com>
2019-01-02 18:00:55 +0100

    Create PayStackProvider.js

---
+++ providers/PayStackProvider.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const { ServiceProvider } = require('@adonisjs/fold')
+
+class PayStackProvider extends ServiceProvider {
+  register () {
+    this.app.singleton('Adonis/Addons/PayStack', () => {
+      const Config = this.app.use('Adonis/Src/Config')
+      const PayStack = require('../src/Paystack')
+      return new PayStack(require('@stitchng/paystack'), Config)
+    })
+  }
+}
+
+module.exports = PayStackProvider
