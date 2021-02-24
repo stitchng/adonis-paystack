0fe196c29393b21777e4a5125242892f4897d5a5
Alpha Kapital MFB <70889093+alphakmfb@users.noreply.github.com>
2021-02-24 18:52:17 +0100

    Update index.js

--- src/PayStack/index.js
+++ src/PayStack/index.js
@@ -1,16 +1,30 @@
 'use strict'
 
-// const Event = use('Event')
+// add testing mock accessibility { paystack-node = v0.2.6+ }
+const MockDecorator = function decorator(context) {
+  this.context = context;
+  this.get = function decorationGetter (target, prop) {
+    if (prop === 'fake' && typeof target[prop] !== 'function') {
+      return this.context.constructor.engageMock;
+    }
+
+    if (prop === 'restore' && typeof target[prop] !== 'function') {
+      return this.context.constructor.disengageMock;
+    }
+
+    return this.context[prop] || target[prop]
+  }
+}
 
 class PayStackAPIClient {
   constructor (Agent, Config, Env) {
     let environment = Env.get('NODE_ENV')
     let apiKey = (environment !== 'production') ? Config.get('paystack.apiTestKey') : Config.get('paystack.apiLiveKey')
 
-    this.agent = new Agent(
-      apiKey,
-      environment
-    )
+    const decorator = new MockDecorator(
+      new Agent(apiKey, environment)
+    );
+    return new Proxy(this, decorator)
   }
 };
 
